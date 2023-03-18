345260790b5ee07dd8c287bd41bd8708317e28a3
Tiger <skdlghwns@gmail.com>
2023-03-18 23:09:37 +0900

    baekjoon N11382 3/18

---
+++ src/main/java/Num11382/Main.java
@@ -0,0 +1,13 @@
+package Num11382;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] N = br.readLine().split(" ");
+        System.out.printf("%d", Long.parseLong(N[0])+Long.parseLong(N[1])+Long.parseLong(N[2]));
+    }
+}
