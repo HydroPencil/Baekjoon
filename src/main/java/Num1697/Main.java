a185035c480a8378d86b656ade2287991f99f378
Tiger <skdlghwns@gmail.com>
2023-05-06 14:28:28 +0900

    baekjoon N1697 5/6 BFS

--- src/main/java/Num1697/Main.java
+++ src/main/java/Num1697/Main.java
@@ -3,42 +3,82 @@ package Num1697;
 import java.io.*;
 import java.util.*;
 
-class Node {
-    int num, level;
-    Node(int num, int level){this.num = num; this.level = level;}
-    public int getNum() {return num;}
-    public int getLevel() {return level;}
-}
-
 public class Main {
     final public static int MAX_POINT = 100001;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] in = br.readLine().split(" ");
-        int su = Math.min(Integer.parseInt(in[0]), Integer.parseInt(in[1])), bro = Math.max(Integer.parseInt(in[0]), Integer.parseInt(in[1])), level = 0;
-        Queue<Node> q = new LinkedList<>();
-        boolean visited[] = new boolean[MAX_POINT]; Arrays.fill(visited, false);
-
-        if(su == bro){
-            System.out.printf("%d", level);
-        } else {
-            q.add(new Node(su, level)); visited[su] = true;
-            while (visited[bro] == false) {
-                Node current = q.poll();
-                if(current.getNum() == bro){level = current.getLevel(); break;}
-                buildNode(current.getNum(), q, visited, current.getLevel());
-                visited[current.getNum()] = true;
+        int su = Integer.parseInt(in[0]), bro = Integer.parseInt(in[1]), level = 0, size;
+        if (su == bro) {System.out.printf("%d", level); return;}
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[MAX_POINT];
+        q.add(su);
+        visited[su] = true;
+        while (!visited[bro]) {
+            size = q.size();
+            for (int i = 0; i < size; i++) {
+                int current = q.poll();
+                visited[current] = true;
+                if (current == bro) {
+                    System.out.printf("%d", level);
+                    return;
+                }
+                if (current * 2 <= 100000 && !visited[current * 2]) {
+                    q.add(current * 2);
+                }
+                if (current - 1 >= 0 && !visited[current - 1]) {
+                    q.add(current - 1);
+                }
+                if (current + 1 <= 100000 && !visited[current + 1]) {
+                    q.add(current + 1);
+                }
             }
-            System.out.printf("%d", level);
+            level++;
         }
         br.close();
     }
-    public static void buildNode(int source, Queue<Node> q, boolean[] visited, int level) {
-        if(source*2 >= 0 && source*2 <= 100000){if(visited[source*2] == false) {q.add(new Node(source*2, level+1));}}
-        if(source-1 >= 0 && source-1 <= 100000){if(visited[source-1] == false) {q.add(new Node(source-1, level+1));}}
-        if(source+1 >= 0 && source+1 <= 100000){if(visited[source+1] == false) {q.add(new Node(source+1, level+1));}}
-    }
 }
+
+
+
+
+//class Node {
+//    int num, level;
+//    Node(int num, int level){this.num = num; this.level = level;}
+//    public int getNum() {return num;}
+//    public int getLevel() {return level;}
+//}
+//
+//public class Main {
+//    final public static int MAX_POINT = 100001;
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String[] in = br.readLine().split(" ");
+//        int su = Integer.parseInt(in[0]), bro = Integer.parseInt(in[1]), level = 0;
+//        Queue<Node> q = new LinkedList<>();
+//        boolean visited[] = new boolean[MAX_POINT]; Arrays.fill(visited, false);
+//
+//        if(su == bro){
+//            System.out.printf("%d", level);
+//        } else {
+//            q.add(new Node(su, level)); visited[su] = true;
+//            while (visited[bro] == false) {
+//                Node current = q.poll();
+//                if(current.getNum() == bro){level = current.getLevel(); break;}
+//                buildNode(current.getNum(), q, visited, current.getLevel());
+//                visited[current.getNum()] = true;
+//            }
+//            System.out.printf("%d", level);
+//        }
+//        br.close();
+//    }
+//    public static void buildNode(int source, Queue<Node> q, boolean[] visited, int level) {
+//        if(source*2 >= 0 && source*2 <= 100000){if(visited[source*2] == false) {q.add(new Node(source*2, level+1));}}
+//        if(source-1 >= 0 && source-1 <= 100000){if(visited[source-1] == false) {q.add(new Node(source-1, level+1));}}
+//        if(source+1 >= 0 && source+1 <= 100000){if(visited[source+1] == false) {q.add(new Node(source+1, level+1));}}
+//    }
+//}
 
 
 //    public static int mino (int in, int bro, int level, int max){
